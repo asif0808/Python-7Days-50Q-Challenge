76bc849b108fc024f65cb730e8bd22443c5e2e4b
Aasif Ansari <116907759+asif0808@users.noreply.github.com>
2025-06-19 09:09:15 +0000

    Completed

--- Day 7/program8.py
+++ Day 7/program8.py
@@ -1,10 +1,10 @@
 # Find first non-repeating character in a string.
 from collections import Counter
-def find(string):
+def find_non_repeating(string):
     freq=Counter(string)
     # freq=dict(freq)     # freq=dict(freq) not needed because its already a dictionary like object
     for s in string:
         if freq[s]==1:
             return s
     return -1
-print(find("aagbbcecdd"))
+print(find_non_repeating("aagbbcecdd"))
